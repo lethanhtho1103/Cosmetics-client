29d0086ef8c9be439dac04b166ab56243e03b50b
Thanh Tho <letanhtho1953@gmail.com>
2024-08-21 18:37:51 +0700

    Tạo footer

--- src/layouts/UserLayout/index.js
+++ src/layouts/UserLayout/index.js
@@ -1,12 +1,14 @@
 import { Container } from '@mui/material';
 import './UserLayout.scss';
 import Header from '~/layouts/components/Header';
+import Footer from '../components/Footer';
 
 function UserLayout({ children }) {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <Header />
       <div className="content">{children}</div>
+      <Footer />
     </Container>
   );
 }
---
+++ src/layouts/components/Footer/index.js
@@ -0,0 +1,106 @@
+import { Container, Grid, Typography, IconButton, Box } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import styles from './Footer.module.scss';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <Box className={styles.wrap} component="footer">
+      <Container disableGutters maxWidth={false} sx={{ width: '1320px', padding: '20px 20px 0px' }}>
+        <Box className={styles.bor1}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={4}>
+              <Typography variant="h4" className={styles.title}>
+                <Link to="/">Orange</Link>
+              </Typography>
+              <Typography className={styles.decs}>
+                Orange là điểm đến lý tưởng cho những ai yêu thích làm đẹp. Chúng tôi cung cấp các sản phẩm mỹ phẩm
+                chính hãng, chất lượng vượt trội và dịch vụ chăm sóc khách hàng tận tâm, giúp bạn có được trải nghiệm
+                mua sắm tuyệt vời và luôn xinh đẹp.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Typography variant="h5" className={styles.title1}>
+                Theo dõi chúng tôi
+              </Typography>
+              <Box className={styles.social}>
+                <IconButton component="a" href="https://www.facebook.com/chiss.thoss?mibextid=ZbWKwL" target="_blank">
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton component="a" href="https://www.instagram.com/_tho1103/" target="_blank">
+                  <InstagramIcon />
+                </IconButton>
+                <IconButton component="a" href="https://www.instagram.com/_tho1103/" target="_blank">
+                  <LinkedInIcon />
+                </IconButton>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <Typography variant="h5" className={styles.title2}>
+                Về chúng tôi
+              </Typography>
+              <Box component="ul" className={styles.list1}>
+                <li>Giới thiệu</li>
+                <li>Liên hệ</li>
+                <li>Chăm sóc khách hàng</li>
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={3}>
+              <Typography variant="h5" className={styles.title2}>
+                Thông tin liên hệ
+              </Typography>
+              <Box component="ul" className={styles.list2}>
+                <li>
+                  <Box className={styles['list2-wrap']}>
+                    <Box className={styles['list2-icon']}>
+                      <EmailIcon />
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" className={styles.title3}>
+                        Email
+                      </Typography>
+                      <span>thob2014791@student.ctu.edu.vn</span>
+                    </Box>
+                  </Box>
+                </li>
+                <li>
+                  <Box className={styles['list2-wrap']}>
+                    <Box className={styles['list2-icon']}>
+                      <PhoneIcon />
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" className={styles.title3}>
+                        Điện thoại
+                      </Typography>
+                      <span>+0972221953</span>
+                    </Box>
+                  </Box>
+                </li>
+                <li>
+                  <Box className={styles['list2-wrap']}>
+                    <Box className={styles['list2-icon']}>
+                      <LocationOnIcon />
+                    </Box>
+                    <Box>
+                      <Typography variant="h6" className={styles.title3}>
+                        Vị trí
+                      </Typography>
+                      <span>Orange, đường 3/2, P. Xuân Khánh, Q. Ninh Kiều, TP. Cần Thơ.</span>
+                    </Box>
+                  </Box>
+                </li>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
+
+export default Footer;

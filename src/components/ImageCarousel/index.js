acbb1f8a7ffb8972e26c2658c8bf2ef4766e7790
Thanh Tho <letanhtho1953@gmail.com>
2024-08-22 15:55:19 +0700

    Tạo Carousel image

---
+++ src/components/ImageCarousel/index.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { Box, MobileStepper, Button } from '@mui/material';
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import img1 from '~/assets/image/img1.png';
+import img2 from '~/assets/image/img2.png';
+import img3 from '~/assets/image/img3.webp';
+import img4 from '~/assets/image/img4.jpg';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    imgPath: img1,
+  },
+  {
+    imgPath: img2,
+  },
+  {
+    imgPath: img3,
+  },
+  {
+    imgPath: img4,
+  },
+];
+
+const ImageCarousel = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ maxWidth: '100%', flexGrow: 1, position: 'relative' }}>
+      <AutoPlaySwipeableViews axis={'x'} index={activeStep} onChangeIndex={handleStepChange} enableMouseEvents>
+        {images.map((step, index) => (
+          <div key={index}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 'auto',
+                  display: 'block',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt=""
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+
+      <Button
+        size="small"
+        onClick={handleBack}
+        disabled={activeStep === 0}
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '12px',
+          transform: 'translateY(-50%)',
+          minWidth: 'auto',
+          backgroundColor: 'white',
+          color: 'black',
+          borderRadius: '50%',
+          '&:hover': {
+            backgroundColor: 'gray',
+          },
+        }}
+      >
+        <KeyboardArrowLeft />
+      </Button>
+
+      <Button
+        size="small"
+        onClick={handleNext}
+        disabled={activeStep === maxSteps - 1}
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          right: '12px',
+          transform: 'translateY(-50%)',
+          minWidth: 'auto',
+          backgroundColor: 'white',
+          color: 'black',
+          borderRadius: '50%',
+          '&:hover': {
+            backgroundColor: 'gray',
+          },
+        }}
+      >
+        <KeyboardArrowRight />
+      </Button>
+
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        sx={{
+          backgroundColor: 'transparent',
+          justifyContent: 'center',
+          position: 'absolute',
+          top: '85%',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+        backButton={null}
+        nextButton={null}
+      />
+    </Box>
+  );
+};
+
+export default ImageCarousel;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,41 @@
+import { Box } from '@mui/material';
 import * as React from 'react';
-import { useSelector } from 'react-redux';
+import ImageCarousel from '~/components/ImageCarousel';
 import UserLayout from '~/layouts/UserLayout';
+import img5 from '~/assets/image/img5.jpg';
+import img6 from '~/assets/image/img6.jpg';
 
 function Home() {
-  const user = useSelector((state) => state.auth.login?.currentUser?.data);
-
   return (
     <UserLayout>
-      <h1>Home page {user?.username}</h1>
+      <Box sx={{ width: '100%', display: 'flex' }}>
+        <Box sx={{ width: '75%' }}>
+          <ImageCarousel />
+        </Box>
+        <Box sx={{ width: '25%', flexDirection: 'column', marginLeft: '4px' }}>
+          <Box
+            component="img"
+            sx={{
+              height: '49.5%',
+              display: 'block',
+              width: '100%',
+              marginBottom: '1%',
+            }}
+            src={img5}
+            alt=""
+          />
+          <Box
+            component="img"
+            sx={{
+              height: '49.5%',
+              display: 'block',
+              width: '100%',
+            }}
+            src={img6}
+            alt=""
+          />
+        </Box>
+      </Box>
     </UserLayout>
   );
 }
